18d4132afcfcf2b6783c106c937a06fba8935c94
Yobsman <76750789+Yobsman@users.noreply.github.com>
2020-12-29 22:00:51 +0200

    Add files via upload

---
+++ List.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 27 20:34:36 2020
+
+@author: -
+"""
+myUniqueList = []
+Var1 = 8
+
+def addThings():
+    myUniqueList.append(Var1)
+    
+    print(myUniqueList)
+addThings()
+myUniqueList.append(addThings())
+
+
+    
+
+
+    
+
